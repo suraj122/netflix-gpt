e841de95bc275c353a1494f0d933c743824f783c
Mukesh kumar <122suraj0@gmail.com>
2023-10-26 17:54:09 +0530

    add login and signup screen ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6">
+      <div className="container mx-auto px-12">
+        <img
+          className="w w-44"
+          src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+          alt=""
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  return (
+    <section className="bg-netflix-img h-screen">
+      <Header />
+      <div className="pb-24">
+        <div className="max-w-md w-full mx-auto bg-black bg-opacity-70 p-12">
+          <form action="">
+            <legend className="font-semibold text-3xl text-white mb-12">
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </legend>
+            {!isSignIn && (
+              <input
+                className="block w-full bg-gray-400 text-white placeholder:text-gray-300 py-2 px-3 rounded-md text-xl mb-5"
+                type="text"
+                placeholder="Full Name"
+              />
+            )}
+
+            <input
+              className="block w-full bg-gray-400 text-white placeholder:text-gray-300 py-2 px-3 rounded-md text-xl"
+              type="email"
+              placeholder="Email"
+            />
+            <input
+              className="block w-full bg-gray-400 text-white placeholder:text-gray-300 py-2 px-3 rounded-md text-xl my-5"
+              type="password"
+              placeholder="Password"
+            />
+            <button className="text-white bg-red-500 rounded-md w-full text-center py-2 px-3 text-xl mt-8">
+              {isSignIn ? "Sign In" : "Sign Up"}
+            </button>
+          </form>
+          <p className="mt-4">
+            <span className="text-gray-300">
+              {isSignIn ? "New to Netflix?" : "Already Registered?"}
+            </span>{" "}
+            <strong
+              className="text-white cursor-pointer"
+              onClick={() => setIsSignIn(!isSignIn)}
+            >
+              {isSignIn ? "Sign up now." : "Sign in now"}
+            </strong>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
